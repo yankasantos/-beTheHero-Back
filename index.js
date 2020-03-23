61d4099aefeb11446aecbc49b3bce5253e21eb15
yankasantos <yankasantos0107@gmail.com>
2020-03-23 15:12:40 -0300

    um json com alguns dados

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request , response) => {
-return response.send('Hello World!');
+    return response.json({
+        evento: 'Semana OmniStack',
+        aluna: 'Yanka Santos'
+    });
 });
 app.listen(3333);
