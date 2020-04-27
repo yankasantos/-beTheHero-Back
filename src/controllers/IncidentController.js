da734efa6ff12c3bf36214c5de81c7246aa7f2cf
yankasantos <yankasantos0107@gmail.com>
2020-04-27 20:13:15 -0300

    implementando metodo delete de um incident

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -19,5 +19,23 @@ module.exports = {
         });
 
         return response.json({ id });
-    }
+    },
+
+    async delete(request,response ){
+        const {id} =  request.params;
+        const ong_id = request.headers.authorization;
+
+        const incidentToDelete = await connection('incidents')
+        .where('id', id)
+        .select('ong_id')
+        .first();
+
+        if(incidentToDelete.ong_id != ong_id){
+            return response.status(401).json({error:'operation not permitted.'});
+        }
+
+        await connection('incidents').where('id', id).delete();
+
+        return response.status(204).send();
+    } 
 };
--- src/routes.js
+++ src/routes.js
@@ -7,13 +7,12 @@ const incidentsController = require("./controllers/IncidentController");
 const routes = express.Router();
 
 routes.get('/ongs', ongController.index);
-
 routes.post('/ongs', ongController.create);
 
 
 routes.get('/incidents', incidentsController.index);
-
 routes.post('/incidents', incidentsController.create);
+routes.delete('/incidents/:id', incidentsController.delete);
 
 
 module.exports = routes;
